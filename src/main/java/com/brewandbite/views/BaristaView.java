8517fb71b0ac5be033884aaf1c36fede30b52937
JSirisavath <Jsirisavath123@gmail.com>
2025-07-18 15:26:52 -0500

    Add views: Barista, CustomerView, and ManagerView.

---
+++ src/main/java/com/brewandbite/views/BaristaView.java
@@ -0,0 +1,31 @@
+
+import com.brewandbite.model.Order;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class BaristaView extends VBox {
+
+    public final ListView<Order> pendingOrders = new ListView<>();
+    public final Button markInProgress = new Button("In Progress");
+    public final Button markReady = new Button("Ready");
+    public final Button refreshBtn = new Button("Refresh");
+    public final HBox actionButtons = new HBox(10, markInProgress, markReady, refreshBtn);
+    private static final String PENDING_ORDER_LABEL = "Pending Order";
+
+    public BaristaView() {
+        setSpacing(15);
+        setPadding(new Insets(10));
+
+        Label pendingOrderHeader = new Label(PENDING_ORDER_LABEL);
+
+        getChildren().addAll(
+                pendingOrderHeader, pendingOrders,
+                actionButtons
+        );
+    }
+}
---
+++ src/main/java/com/brewandbite/views/CustomerView.java
@@ -0,0 +1,39 @@
+
+import com.brewandbite.model.MenuItem;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class CustomerView extends VBox {
+
+    public final ListView<MenuItem> menuList = new ListView<>();
+    public final ListView<MenuItem> cartList = new ListView<>();
+    public final Button placeOrder = new Button("Place Order");
+    public final Button clearOrder = new Button("Clear Order");
+    public final HBox orderButtons = new HBox(10, placeOrder, clearOrder);
+    public final TextField nameField = new TextField();
+    private static final String CUSTOMER_NAME_LABEL = "Customer Name:";
+    private static final String MENU_LABEL = "Menu";
+    private static final String YOUR_CART_LABEL = "Your Cart";
+
+    public CustomerView() {
+        setSpacing(15);
+        setPadding(new Insets(10));
+
+        Label customerNameHeader = new Label(CUSTOMER_NAME_LABEL);
+        Label menuHeader = new Label(MENU_LABEL);
+        Label cartHeader = new Label(YOUR_CART_LABEL);
+
+        getChildren().addAll(
+                customerNameHeader, nameField,
+                menuHeader, menuList,
+                orderButtons,
+                cartHeader, cartList
+        );
+    }
+}
